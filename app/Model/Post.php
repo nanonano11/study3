8e2e360a510e4a24cceeb3e4558688b50ef0e094
nanonano11 <nano3nano2nano5@gmail.com>
2018-06-03 14:00:06 +0100

    0603

--- app/Model/Post.php
+++ app/Model/Post.php
@@ -1,2 +1,11 @@
+<?php
 class Post extends AppModel {
+    public $validate = array(
+        'title' => array(
+            'rule' => 'notBlank'
+        ),
+        'body' => array(
+            'rule' => 'notBlank'
+        )
+    );
 }
