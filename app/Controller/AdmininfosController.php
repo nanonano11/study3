bea0c925437b25468cac8dfd1eebfd158d1d77b2
nanonano11 <nano3nano2nano5@gmail.com>
2018-06-11 05:23:16 +0100

    0611

--- app/Controller/AdmininfosController.php
+++ app/Controller/AdmininfosController.php
@@ -6,74 +6,95 @@ class AdmininfosController extends PassesController {
 
   public $uses = array('Info');
 
-    public $helpers = array('Html', 'Form', 'Flash');
-    // public $components = array('Flash');
+  public $helpers = array('Html', 'Form', 'Flash');
+  // public $components = array('Flash');
 
-    public function index() {
-        $this->set('infos', $this->Info->find('all'));
-    }
+  public function index() {
+    $this->set('infos', $this->Info->find('all'));
+  }
 
-    public function view($id) {
-        if (!$id) {
-            throw new NotFoundException(__('Invalid post'));
-        }
-
-        $info = $this->Info->findById($id);
-        if (!$info) {
-            throw new NotFoundException(__('Invalid post'));
-        }
-        $this->set('info', $info);
-    }
-
-    public function add() {
-        if ($this->request->is('post')) {
-            $this->Info->create();
-            if ($this->Info->save($this->request->data)) {
-                $this->Flash->success(__('Your post has been saved.'));
-                return $this->redirect(array('action' => 'index'));
-            }
-            $this->Flash->error(__('Unable to add your post.'));
-        }
-    }
-    public function edit($id = null) {
+  public function view($id) {
     if (!$id) {
-        throw new NotFoundException(__('Invalid post'));
+      throw new NotFoundException(__('Invalid post'));
     }
 
     $info = $this->Info->findById($id);
     if (!$info) {
-        throw new NotFoundException(__('Invalid post'));
+      throw new NotFoundException(__('Invalid post'));
+    }
+    $this->set('info', $info);
+  }
+
+  public function add() {
+    if ($this->request->is('post')) {
+      $this->Info->create();
+      if ($this->Info->save($this->request->data)) {
+        $this->Flash->success(__('Your post has been saved.'));
+        return $this->redirect(array('action' => 'index'));
+      }
+      $this->Flash->error(__('Unable to add your post.'));
+    }
+  }
+  public function edit($id = null) {
+    if (!$id) {
+      throw new NotFoundException(__('Invalid post'));
+    }
+
+    $info = $this->Info->findById($id);
+    if (!$info) {
+      throw new NotFoundException(__('Invalid post'));
     }
 
     if ($this->request->is(array('post', 'put'))) {
-        $this->Info->id = $id;
-        if ($this->Info->save($this->request->data)) {
-            $this->Flash->success(__('Your post has been updated.'));
-            return $this->redirect(array('action' => 'index'));
-        }
-        $this->Flash->error(__('Unable to update your post.'));
+      $this->Info->id = $id;
+      if ($this->Info->save($this->request->data)) {
+        $this->Flash->success(__('Your post has been updated.'));
+        return $this->redirect(array('action' => 'index'));
+      }
+      $this->Flash->error(__('Unable to update your post.'));
     }
 
     if (!$this->request->data) {
-        $this->request->data = $info;
+      $this->request->data = $info;
     }
-}
-public function delete($id) {
+  }
+  public function delete($id) {
     if ($this->request->is('get')) {
-        throw new MethodNotAllowedException();
+      throw new MethodNotAllowedException();
     }
 
     if ($this->Info->delete($id)) {
-        $this->Flash->success(
-            __('The post with id: %s has been deleted.', h($id))
-        );
+      $this->Flash->success(
+        __('The post with id: %s has been deleted.', h($id))
+      );
     } else {
-        $this->Flash->error(
-            __('The post with id: %s could not be deleted.', h($id))
-        );
+      $this->Flash->error(
+        __('The post with id: %s could not be deleted.', h($id))
+      );
     }
 
     return $this->redirect(array('action' => 'index'));
-}
-
+  }
+  public function search(){
+    if($this->request->is('post') && $this->request->data['Search']['title'] != ""){
+      //Formの値を取得
+      $title = $this->request->data['Info']['title'];
+      //検索文字を空白（全角又は半角）で区切って配列$keywordsに代入
+      $keywords = preg_split("/ |\\s/",$title);
+      //配列$keywordsの数だけ繰り返して検索条件を$conditionsに代入
+      foreach($keywords as $keyword){
+        $conditions[] = "title like '%$keyword%'";
+      }
+      //POSTされたデータを曖昧検索
+      $data = $this->Info->find('all',array(
+        'conditions' => $conditions
+      ));
+      $this->set('datas',$data);
+    }else{
+      //POST以外の場合
+      //一覧表示
+      $data = $this->Info->find('all');
+      $this->set('datas',$data);
+    }
+  }
 }
--- app/View/Admininfos/search.ctp
+++ app/View/Admininfos/search.ctp
@@ -1,35 +1,6 @@
 
-admininfoscontroller
 <?php
-public function search(){
- //リクエストがPOSTで送られたデータが空白で無ければ
-   if($this->request->is('post') && $this->request->data['Search']['title'] != ""){
-      //Formの値を取得
-      $title = $this->request->data['Post']['title'];
-    //検索文字を空白（全角又は半角）で区切って配列$keywordsに代入
-    $keywords = preg_split("/ |\\s/",$title);
-    //配列$keywordsの数だけ繰り返して検索条件を$conditionsに代入
-    foreach($keywords as $keyword){
-      $conditions[] = "title like '%$keyword%'";
-    }
-    //POSTされたデータを曖昧検索
-    $data=$this->->find('all',array(
-     'conditions' => $conditions
-    ));
-     $this->set('datas',$data);
-   }else{
-     //POST以外の場合
-     //一覧表示
-     $data=$this->Post->find('all');
-     $this->set('datas',$data);
-   }
- }
--------------------------------------------------------
-
-
-
-<?php
- echo $this->Form->create('Post', array('action'=>'search'));
+ echo $this->Form->create('Info', array('type'=>'post','url'=>'search'));
  echo $this->Form->input('title', array('label' => 'タイトル名を入れてください'));
  echo $this->Form->end('検索');
 ?>
@@ -42,9 +13,9 @@ public function search(){
  </tr>
  <?php foreach ($datas as $data): ?>
  <tr>
-    <td><?php echo h($data['Post']['id']); ?></td>
-    <td><?php echo $this->Html->link($data['Post']['title'],array('action'=>'edit',h($data['Post']['id'])));?></td>
-    <td><?php echo h($data['Post']['created']); ?></td>
+    <td><?php echo h($data['Info']['id']); ?></td>
+    <td><?php echo $this->Html->link($data['Info']['title'],array('action'=>'edit',h($data['Info']['id'])));?></td>
+    <td><?php echo h($data['Info']['created']); ?></td>
  </tr>
 <?php endforeach; ?>
  </table>
